46fad5c4f858e4732a2313706fe5e3a0812a4de2
Hadrien Patte <hadrien.patte@protonmail.com>
2019-12-26 16:22:34 +0100

    Feat: add post script
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+git init
+git add .
+git commit -m "Initialize {{ cookiecutter.pretty_role_name }} repository"
+
+read -p "Create a repository (HadrienPatte/ansible-role-{{ cookiecutter.hyphenated_role_name }}) on Github ? (Y/n) " ANSWER
+
+if [ -z "$ANSWER" -o "$ANSWER" = "Y" -o "$ANSWER" = "y" ]
+then
+    if ! [ -x "$(command -v hub)" ]
+    then
+        echo "Hub is not installed. You'll need to create the GitHub repository manually." >&2
+    else
+        hub create -p "HadrienPatte/ansible-role-{{ cookiecutter.hyphenated_role_name }}"
+        git push -u origin master
+    fi
+fi
